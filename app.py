716a3ad43bd9ad7dba6e24647ea97020b093ec77
Farhan Rahman <farhan.nobody@outlook.com>
2017-01-10 14:09:39 -0500

    Button Click Test

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
-import RPi.GPIO as GPIO ## Import GPIO library
-import time ## Import 'time' library. Allows us to use 'sleep'
+import RPi.GPIO as GPIO
+import time
+prev_input = 0
 
-GPIO.setmode(GPIO.BCM) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.IN)
 
-##Define a function named Blink()
-def Blink(numTimes,speed):
-
-    for i in range(0,numTimes):## Run loop numTimes
-        print "Iteration " + str(i+1)## Print current loop
-        GPIO.output(7,True)## Switch on pin 7
-        time.sleep(speed)## Wait
-        GPIO.output(7,False)## Switch off pin 7
-        time.sleep(speed)## Wait
-        print "Done" ## When loop is complete, print "Done"
-
-GPIO.cleanup()
-
-## Ask user for total number of blinks and length of each blink
-iterations = raw_input("Enter total number of times to blink: ")
-speed = raw_input("Enter length of each blink(seconds): ")
-
-## Start Blink() function. Convert user input from strings to numeric data types and pass to Blink() as parameters
-Blink(int(iterations),float(speed))
+while True:
+  #take a reading
+  input = GPIO.input(17)
+  #if the last reading was low and this one high, print
+  if ((not prev_input) and input):
+    print("Button pressed")
+  #update previous input
+  prev_input = input
+  #slight pause to debounce
+  time.sleep(0.05)
